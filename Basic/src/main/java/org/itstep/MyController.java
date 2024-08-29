5f4d01dd9b461506f7f3c148976622eef48e95a6
BespalovaAnastasiya123 <3537184@gmail.com>
2024-08-29 21:24:55 +0300

    29/08/2024

---
+++ Basic/src/main/java/org/itstep/MyController.java
@@ -0,0 +1,18 @@
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @PreAuthorize("hasAuthority('write')")
+    @GetMapping("/root")
+    public String root(){
+        return "root";
+    }
+
+    @PreAuthorize("hasAnyAuthority('write', 'user')")
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+}
